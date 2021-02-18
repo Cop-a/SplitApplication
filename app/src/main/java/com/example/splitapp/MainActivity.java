19c3cf152fba554decedb9ecf64165d40fa0b1f5
coppersmitha <coppersmitha@mail.usf.edu>
2021-02-18 18:12:03 -0500

    Login with Firebase. DK

--- app/src/main/java/com/example/splitapp/MainActivity.java
+++ app/src/main/java/com/example/splitapp/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.splitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private TextView Info;
+    private TextView userRegistration;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        userRegistration = (TextView)findViewById(R.id.tvRegister);
+
+
+
+        Info.setText("Please Login.");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+       if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+           Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+           startActivity(intent);
+       }
+       else{
+            Info.setText("Incorrect Username or Password. Try Again.");
+       }
     }
 }
