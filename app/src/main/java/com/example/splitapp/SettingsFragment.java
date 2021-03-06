9c19e8bf0d340b6656e5091734441202d58a7e94
Cole O'Brien <coleobrien@usf.edu>
2021-03-06 16:08:49 -0500

    Dark mode / settings

--- app/src/main/java/com/example/splitapp/SettingsFragment.java
+++ app/src/main/java/com/example/splitapp/SettingsFragment.java
@@ -1,19 +1,47 @@
 package com.example.splitapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 
 public class SettingsFragment extends Fragment {
+
+    private SwitchCompat mySwitch;
+    View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         return inflater.inflate(R.layout.fragment_settings, container, false);
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        mySwitch = view.findViewById(R.id.switch1);
+
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    mySwitch.setChecked(isChecked);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    mySwitch.setChecked(isChecked);
+                }
+
+            }
+        });
+        return view;
     }
 }
